06bdc554d406795551847c4466d160bb548b8a65
gcbragion@gmail.com <gcbragion@gmail.com>
2022-07-12 21:02:25 -0300

    Changing file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { serverHttp } from "./app";
 
-serverHttp.listen(4000, () => 
-    console.log('Server is running on PORT 4000')
+serverHttp.listen(process.env.PORT, () => 
+    console.log(`Server is running on PORT ${process.env.PORT}`)
 );
